be364dbb397309c3fb33e723bbde3636e65c9a9f
Nattapon <nattapon@ava.fund>
2025-07-14 16:17:29 +0700

    initial commit

---
+++ SwiftUIPlayground/SwiftUIPlayground/SwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlaygroundApp.swift
+//  SwiftUIPlayground
+//
+//  Created by IntrodexMini on 14/7/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIPlayground/SwiftUIPlaygroundTests/SwiftUIPlaygroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlaygroundTests.swift
+//  SwiftUIPlaygroundTests
+//
+//  Created by IntrodexMini on 14/7/2568 BE.
+//
+
+import Testing
+@testable import SwiftUIPlayground
+
+struct SwiftUIPlaygroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
